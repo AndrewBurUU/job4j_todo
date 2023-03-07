a01cd629d65db3b2ddcb7d8600cd3625e6756220
AndrewBurUU <andriv80@gmail.com>
2023-03-07 08:29:40 +0800

    0. ToOne [#6873 #397853]

--- db/scripts/001_dml_insert_tasks.sql
+++
@@ -1,3 +0,0 @@
-insert into tasks (description, created, done) values ('task1', CURRENT_DATE, false);
-insert into tasks (description, created, done) values ('task2', CURRENT_DATE - 2, true);
-insert into tasks (description, created, done) values ('task3', CURRENT_DATE - 3, false);
